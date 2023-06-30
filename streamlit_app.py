536a4f9fc6ecad962551ecec6a724476f09e996b
hushal-parmar-ld <121931664+hushal-parmar-ld@users.noreply.github.com>
2023-06-30 12:44:18 +0530

    Update streamlit_app.py
    imported requests tested the response and added advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import requests
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -19,3 +22,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
